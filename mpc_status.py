1d3765b55508609c7a3e1b07111c9942642d19b7
Spiel0r <mario.spielhofer@gmail.com>
2024-05-10 20:13:45 +0200

    mpc_status.py created

---
+++ mpc_status.py
@@ -0,0 +1,133 @@
+#
+MPC-Status wurde in die index.html geschrieben.
+MPC-Status wurde in die index.html geschrieben.
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:14:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:15:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:16:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:17:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:18:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:19:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:20:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status wurde in die index.html geschrieben.
+Fri 10 May 19:21:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:22:02 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:23:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:24:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:25:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:26:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:27:01 CEST 2024 - Ausführung von mpc_status.py
+MPD error: Connection refused
+Traceback (most recent call last):
+  File "/home/admin/orpheus/mpc_status.py", line 33, in <module>
+    mpc_output = get_mpc_status()
+                 ^^^^^^^^^^^^^^^^
+  File "/home/admin/orpheus/mpc_status.py", line 8, in get_mpc_status
+    output = subprocess.check_output(['mpc', 'status']).decode('utf-8')
+             ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/usr/lib/python3.11/subprocess.py", line 466, in check_output
+    return run(*popenargs, stdout=PIPE, timeout=timeout, check=True,
+           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  File "/usr/lib/python3.11/subprocess.py", line 571, in run
+    raise CalledProcessError(retcode, process.args,
+subprocess.CalledProcessError: Command '['mpc', 'status']' returned non-zero exit status 1.
+Fri 10 May 19:28:02 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:29:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:30:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:31:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:32:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:33:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:34:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:35:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:36:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:37:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:38:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:39:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:40:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:41:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:42:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:43:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:44:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:45:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:46:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:47:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:48:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:49:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:50:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:51:02 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:52:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:53:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:54:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:55:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:56:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:57:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:58:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 19:59:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:00:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:01:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:02:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:03:02 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:04:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:05:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:06:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:07:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:08:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:09:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:10:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
+Fri 10 May 20:11:01 CEST 2024 - Ausführung von mpc_status.py
+MPC-Status und MPD-Status wurden in die index.html geschrieben.
